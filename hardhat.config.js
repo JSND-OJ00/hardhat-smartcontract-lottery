f97b78f3b14f572dcb4361f611e9463484d2d8ea
JSND-OJ00 <jsnd.oj@gmail.com>
2022-07-14 21:38:53 +0900

    contract done(hoprfully), script wip.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,40 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = precess.env.RINKEBY_RPC_URL
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY ||
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks:{
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    rinkeby: {
+      chainId: 4,
+      blockConfirmations: 6,
+      url: RINKEBY_RPC_URL,
+      accounts: '',
+    }
+  },
   solidity: "0.8.8",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default:1,
+    }
+  }
 };
